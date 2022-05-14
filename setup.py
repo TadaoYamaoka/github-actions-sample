e0cf1e2d133212c76b2b83237c714989cebd749f
TadaoYamaoka <tadaoyamaoka@gmail.com>
2022-05-15 00:32:27 +0900

    v0.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ ext_modules = [
 
 setup(
     name='ty_github-actions-sample',
-    version='0.0.1',
+    version='0.0.2',
     packages=['mysample'],
     ext_modules=ext_modules,
     cmdclass={'build_ext': my_build_ext},
